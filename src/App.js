e0a474ba56b20c36bb2ba6ef3cb0b401ec57e174
Ben Sparks <bensparks0@gmail.com>
2021-02-11 23:48:06 +0200

    Setting up hooks and Firebase api key code

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React, {  } from 'react'
+import React, { } from 'react'
 import './App.css'
 
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
 
-import {useAuthState} from 'react-firebase-hooks/auth'
-import {useCollectionData} from 'react-firebase-hooks/firestore'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { useCollectionData } from 'react-firebase-hooks/firestore'
+
+const API_KEY = env.process.REACT_APP_FIREBASE_API
 
 firebase.initializeApp({
-  // config
+  apiKey: API_KEY,
+  authDomain: "chat-party-60343.firebaseapp.com",
+  projectId: "chat-party-60343",
 })
+
+const auth
 
 
 const App = () => {
